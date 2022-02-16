4aee1e23b091ba0a9c53a1aee37139429bd0a9e4
JisanFahim <fahimjason@users.noreply.github.com>
2022-02-16 21:30:29 +0600

    Done display product.

--- controllers/admin.js
+++ controllers/admin.js
@@ -6,7 +6,7 @@ exports.getAddProduct = (req, res, next) => {
 
 exports.postAddProduct = (req, res, next) => {
     const { title, imageUrl, price, description } = req.body;
-    const product = new Product(title, imageUrl, price, description);
+    const product = new Product(title, imageUrl, description, price);
     product.save();
     res.redirect('/');
 }
