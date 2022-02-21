1beb2c5549459a83540f4be081b41a1ce13d5988
JisanFahim <fahimjason@users.noreply.github.com>
2022-02-21 19:04:45 +0600

    Added product update function.

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,14 +1,43 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-    res.render('admin/add-product', { pageTitle: 'Add Product', path: '/admin/add-product' });
+    res.render('admin/add-product', {
+        pageTitle: 'Add Product',
+        path: '/admin/add-product',
+        editing: false
+    });
 }
 
 exports.postAddProduct = (req, res, next) => {
     const { title, imageUrl, price, description } = req.body;
-    const product = new Product(title, imageUrl, description, price);
+    const product = new Product(null, title, imageUrl, description, price);
     product.save();
     res.redirect('/');
+}
+
+exports.getEditProduct = (req, res, next) => {
+    const editMode = req.query.edit;
+    const productId = req.params.productId;
+
+    if (!editMode) return res.redirect('/');
+
+    Product.findById(productId, product => {
+        if (!product) return res.redirect('/');
+
+        res.render('admin/edit-product', {
+            pageTitle: 'Edit Product',
+            path: '/admin/edit-product',
+            editing: editMode,
+            product: product
+        });
+    });
+}
+
+exports.postEditProduct = (req, res, next) => {
+    const { productId, title, imageUrl, price, description } = req.body;
+    const updatedProduct = new Product(productId, title, imageUrl, description, price);
+    updatedProduct.save();
+    res.redirect('/admin/products')
 }
 
 exports.getProducts = (req, res, next) => {
