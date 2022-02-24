f7ff35ba0a762db8dbd874fefa94772063638979
JisanFahim <fahimjason@users.noreply.github.com>
2022-02-24 18:46:02 +0600

    Refactored MongoDB conection.

--- models/product.js
+++ models/product.js
@@ -1,5 +1,18 @@
-const Sequelize = require('sequelize');
-const sequelize = require('../util/database');
+const getDb = require('../util/database').getDb;
+
+module.exports = class Product {
+    constructor(id, title, imageUrl, description, price) {
+        this.id = id;
+        this.title = title;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.price = price;
+    }
+
+    save() {
+
+    }
+}
 
 const Product = sequelize.define('products', {
     id: {
--- util/database.js
+++ util/database.js
@@ -1,13 +1,25 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = callback => {
     MongoClient.connect('mongodb+srv://nodeCourse:nodeCourse123@cluster0.4i8kb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
         .then(client => {
             console.log('Connected');
-            callback(client);
+            _db = client.db();
+            callback();
         })
-        .catch(err => console.log(err));
+        .catch(err => {
+            console.log(err);
+            throw err;
+        });
 }
 
-module.exports = mongoConnect;
+const getDb = () => {
+    if (_db) return _db;
+    throw 'Database not found!'
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
