85b49c1708c28b1428bb4f48566258b763ca10e2
JisanFahim <fahimjason@users.noreply.github.com>
2022-03-01 22:15:38 +0600

    Added MongoDB in cart.

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
     User.findById('6217f86ceb6455d24aad31c4')
         .then(user => {
-            req.user = user;
+            req.user = new User(user.name, user.email, user.cart, user._id);
             next();
         })
         .catch(err => console.log(err));
--- models/user.js
+++ models/user.js
@@ -1,11 +1,13 @@
 const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
-const objectId = mongodb.ObjectId;
+const ObjectId = mongodb.ObjectId;
 class User {
-    constructor(username, email) {
+    constructor(username, email, cart, id) {
         this.name = username;
         this.email = email;
+        this.cart = cart;
+        this._id = id;
     }
 
     save() {
@@ -16,10 +18,32 @@ class User {
             .catch(err => console.log(err));
     }
 
+    addToCart(product) {
+        const cartProductIndex = this.cart.items.findIndex(cp => cp.productId.toString() === product._id.toString());
+        const updatedCartItems = [...this.cart.items];
+        let newQuantity = 1;
+
+        if (cartProductIndex >= 0) {
+            newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+            updatedCartItems[cartProductIndex].quantity = newQuantity;
+        } else {
+            updatedCartItems.push({ productId: new ObjectId(product._id), quantity: newQuantity });
+        }
+
+        const updatedCart = { items: updatedCartItems };
+        const db = getDb();
+
+        return db.collection('users')
+            .updateOne(
+                { _id: new ObjectId(this._id) },
+                { $set: { cart: updatedCart } }
+            );
+    }
+
     static findById(userId) {
         const db = getDb();
 
-        return db.collection('users').findOne({ _id: new objectId(userId) });
+        return db.collection('users').findOne({ _id: new ObjectId(userId) });
     }
 }
 
