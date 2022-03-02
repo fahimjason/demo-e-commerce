1214b5175da26972843b12de2b3c86016cc5877b
JisanFahim <fahimjason@users.noreply.github.com>
2022-03-02 19:40:23 +0600

    Added MongoDB for order.

--- controllers/shop.js
+++ controllers/shop.js
@@ -75,30 +75,14 @@ exports.postCartDeleteProduct = (req, res, next) => {
 exports.postOrder = (req, res, next) => {
     let fetchedCart;
 
-    req.user.getCart()
-        .then(cart => {
-            fetchedCart = cart;
-            return cart.getProducts()
-        })
-        .then(products => {
-            return req.user.createOrder()
-                .then(order => {
-                    order.addProducts(products.map(product => {
-                        product.orderItems = { quantity: product.cartItems.quantity };
-                        return product;
-                    }))
-                })
-                .catch(err => console.error(err));
-        })
-        .then(result => fetchedCart.setProducts(null))
+    req.user.addOrder()
         .then(result => res.redirect('/orders'))
         .catch(err => console.log(err));
 }
 
 exports.getOrders = (req, res, next) => {
-    req.user.getOrders({ include: ['products'] })
+    req.user.getOrders()
         .then(orders => {
-            console.log(orders)
             res.render('shop/orders', {
                 pageTitle: 'Your orders',
                 path: '/orders',
