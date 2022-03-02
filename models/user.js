57d4b582f994f510b88105198a3fea2dd172a277
JisanFahim <fahimjason@users.noreply.github.com>
2022-03-02 12:33:27 +0600

    Displayed cart.

--- models/user.js
+++ models/user.js
@@ -40,6 +40,22 @@ class User {
             );
     }
 
+    getCart() {
+        const db = getDb();
+        const productIds = this.cart.items.map(i => i.productId);
+
+        return db.collection('products')
+            .find({ _id: { $in: productIds } })
+            .toArray()
+            .then(products => products.map(p => {
+                console.log(p);
+                return {
+                    ...p,
+                    quantity: this.cart.items.find(i => i.productId.toString() === p._id.toString()).quantity
+                }
+            }));
+    }
+
     static findById(userId) {
         const db = getDb();
 
