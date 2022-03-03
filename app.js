28db0ff1fd1f18bea58b17da5baa42cad6fa88bf
JisanFahim <fahimjason@users.noreply.github.com>
2022-03-03 11:37:21 +0600

    Added mongoose.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
