10f34a6024a95944019677d0bcec83f2f2bb688c
JisanFahim <fahimjason@users.noreply.github.com>
2022-03-05 05:12:12 +0600

    Added Mongoose in cart.

--- controllers/shop.js
+++ controllers/shop.js
@@ -39,8 +39,10 @@ exports.getIndex = (req, res, next) => {
 }
 
 exports.getCart = (req, res, next) => {
-    req.user.getCart()
-        .then(products => {
+    req.user.populate('cart.items.productId')
+        .then(user => {
+            const products = user.cart.items;
+
             res.render('shop/cart', {
                 pageTitle: 'Your Cart',
                 path: '/cart',
@@ -64,7 +66,7 @@ exports.postCart = (req, res, next) => {
 exports.postCartDeleteProduct = (req, res, next) => {
     const prodId = req.body.productId;
 
-    req.user.deleteItemsFromCart(prodId)
+    req.user.removeFromCart(prodId)
         .then(result => res.redirect('/cart'))
         .catch(err => console.log(err));
 }
