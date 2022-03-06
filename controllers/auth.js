2cc721377a0088ba83c2d24e14194ca981fa5d2c
JisanFahim <fahimjason@users.noreply.github.com>
2022-03-06 23:58:50 +0600

    Implemented error message with connect-flash.

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,19 +3,34 @@ const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
+    let message = req.flash('error');
+
+    if (message.length > 0) {
+        message = message[0];
+    } else {
+        message = null;
+    }
 
     res.render('auth/login', {
         pageTitle: 'Login',
         path: '/login',
-        isAuthenticated: false
+        errorMessage: message
     });
 }
 
 exports.getSignup = (req, res, next) => {
+    let message = req.flash('error');
+
+    if (message.length > 0) {
+        message = message[0];
+    } else {
+        message = null;
+    }
+
     res.render('auth/signup', {
         path: '/signup',
         pageTitle: 'Signup',
-        isAuthenticated: false
+        errorMessage: message
     });
 };
 
@@ -24,7 +39,10 @@ exports.postLogin = (req, res, next) => {
 
     User.findOne({ email: email })
         .then(user => {
-            if (!user) return res.redirect('/login');
+            if (!user) {
+                req.flash('error', 'Invalid email or password.');
+                return res.redirect('/login')
+            };
 
             bcrypt.compare(password, user.password)
                 .then(doMatch => {
@@ -36,6 +54,7 @@ exports.postLogin = (req, res, next) => {
                             res.redirect('/');
                         });
                     }
+                    req.flash('error', 'Invalid email or password.');
                     res.redirect('/login');
                 })
                 .catch(err => {
@@ -54,7 +73,10 @@ exports.postSignup = (req, res, next) => {
 
     User.findOne({ email: email })
         .then(userDoc => {
-            if (userDoc) return res.redirect('/signup');
+            if (userDoc) {
+                req.flash('error', 'E-Mail exists already, please pick a different one.');
+                return res.redirect('/signup');
+            }
             return bcrypt.hash(password, 12)
                 .then(hashedPassword => {
                     const user = new User({
