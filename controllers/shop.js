c501ea08fe1a28a023d7c20d185949ed03ea1600
JisanFahim <fahimjason@users.noreply.github.com>
2022-03-10 18:02:57 +0600

    Added Stripe for payment.

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
-
 const PDFDocument = require('pdfkit');
+const stripe = require('stripe')('sk_test_51Ie3IHAgyIUkUwiGJHxAtAXeIEIt8EueaFZ2Z8Hpn3wf7jqtir5DZjh5LG0p3yNLGcXbQP4Me4VxgXNrlewUAuVL00JQjnz4lG');
 
 const Product = require('../models/product');
 const Order = require('../models/order');
@@ -135,7 +135,51 @@ exports.postCartDeleteProduct = (req, res, next) => {
         });
 }
 
-exports.postOrder = (req, res, next) => {
+exports.getCheckout = (req, res, next) => {
+    let products;
+    let total = 0;
+
+    req.user.populate('cart.items.productId')
+        .then(user => {
+            products = user.cart.items;
+            total = 0;
+
+            products.forEach(p => {
+                total += p.quantity * p.productId.price;
+            });
+
+            return stripe.checkout.sessions.create({
+                payment_method_types: ['card'],
+                line_items: products.map(p => {
+                    return {
+                        name: p.productId.title,
+                        description: p.productId.description,
+                        amount: p.quantity * 100,
+                        currency: "usd",
+                        quantity: p.quantity
+                    }
+                }),
+                success_url: req.protocol + '://' + req.get('host') + '/checkout/success',      // => http://localhost:3000
+                cancel_url: req.protocol + '://' + req.get('host') + '/checkout/success'
+            });
+        })
+        .then(session => {
+            res.render('shop/checkout', {
+                path: '/checkout',
+                pageTitle: 'Checkout',
+                products: products,
+                totalSum: total,
+                sessionId: session.id
+            });
+        })
+        .catch(err => {
+            const error = new Error(err);
+            error.httpStatusCode = 500;
+            return next(error);
+        });
+};
+
+exports.getCheckoutSuccess = (req, res, next) => {
     req.user.populate('cart.items.productId')
         .then(user => {
             const products = user.cart.items.map(item => {
